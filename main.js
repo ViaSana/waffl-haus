f3c99c4abcddec9ac711d178e691549b3c50946a
playfluent <joe@playfluent.com>
2023-07-23 18:40:44 +0800

    feature/memberspace
    not sure if it still works

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ mainHeading.forEach(object => {
         duration:1,
     })
 })
+
+function toggleDiv() {
+    var div = document.getElementById("myDiv");
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
